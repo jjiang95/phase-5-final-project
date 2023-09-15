b001b9df4cd83ee47ae97416550a2e5d43e871e3
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-09-15 13:58:09 -0400

    first commit

---
+++ server/models.py
@@ -0,0 +1,15 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    created = db.Column(db.DateTime, server_default=db.func.now())
+
+    def __repr__(self):
+        return f'User: {self.name}'
