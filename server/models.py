9ae0162d6fb8c6da9ed25a6ec865d99b012c3e81
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-09-22 14:00:12 -0400

    adds Posts table and updates seed.py

--- server/models.py
+++ server/models.py
@@ -5,12 +5,15 @@ from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
-
+    
+    serialize_rules = ('-posts.user',)
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     _password = db.Column(db.String, nullable=False)
     created = db.Column(db.DateTime, default=db.func.now())
     admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    posts = db.relationship('Post', backref='user')
 
     @hybrid_property
     def password(self):
@@ -31,10 +34,11 @@ class User(db.Model, SerializerMixin):
     def __repr__(self):
         return f'User: {self.username} | Created: {self.created}'
     
-class Posts(db.Model, SerializerMixin):
+class Post(db.Model, SerializerMixin):
     __tablename__ = 'posts'
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(600), nullable=False)
-    likes = db.Column(db.Integer, default=0, nullable=False)
+    likes = db.Column(db.Integer, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
--- server/seed.py
+++ server/seed.py
@@ -1,16 +1,22 @@
 from app import app
-from models import User
+from models import User, Post
 from config import db
 
 if __name__ == '__main__':
     with app.app_context():
         User.query.delete()
+        Post.query.delete()
 
         user1 = User(username='xijiang', admin=True)
         user1.password = '123456'
         user2 = User(username='jjiang95')
         user2.password = '12345678'
 
+        post1 = Post(content='Mary had a little lamb', user_id=1)
+        post2 = Post(content="Baa baa black sheep", user_id=2)
+        
         db.session.add(user1)
         db.session.add(user2)
+        db.session.add(post1)
+        db.session.add(post2)
         db.session.commit()
